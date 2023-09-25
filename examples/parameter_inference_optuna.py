d1d1378c471bc5540b5af4addc2da848f0c5f886
Cristian Beltran <cristianbehe@gmail.com>
2023-09-25 12:58:44 +0900

    Update License

--- examples/parameter_inference_optuna.py
+++ examples/parameter_inference_optuna.py
@@ -1,24 +1,37 @@
-# Copyright (c) 2021 NVIDIA CORPORATION.  All rights reserved.
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2023, OMRON SINIC X
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of OMRON SINIC X nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Cristian C. Beltran-Hernandez, Nicolas Erbetti
 
-"""
-This example demonstrates how to leverage the differentiability of DiSECt to
-infer the simulation parameters of our simulator to reduce the gap between
-the simulated knife force profile and a ground-truth force profile.
-
-The optimization progress is logged in tensorboard, which can be viewed by
-running the following command:
-    tensorboard --logdir=log
-
-Note: this example currently allocates about 6 GB of GPU memory, which we aim to
-reduce in further updates of our simulator.
-"""
-
-# fmt: off
 import torch
 torch.autograd.set_detect_anomaly(False, check_nan=True)
 torch.set_printoptions(threshold=10000)
@@ -31,7 +44,6 @@ import pickle
 from tensorboardX import SummaryWriter
 
 from disect.cutting import load_settings, save_settings, optuna_trainer, adam_trainer, create_sim
-# fmt: on
 
 settings = load_settings("examples/config/cooking/ansys_cucumber.json")
 
